901c24370bab3489bd3d9d2bbe5972b7c4d70dfc
Kolicks <ricardcasas@yahoo.es>
2022-03-30 20:31:41 +0200

    mooore

--- code/express-discorder/server.js
+++ code/express-discorder/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require("axios").default;
 
 const app = express();
-const port = 3000;
+const port = 40300;
 
 app.use(express.json());
 
@@ -19,7 +19,7 @@ app.get("/", (req, res) => res.send(`
 
 app.post("/github", (req, res) => {
   const content = ":wave: Hi mom!";
-  const avatarUrl = "https://media.giphy.com/media/3o7TKSjRrfIPjeiVyM/giphy.gif";
+  const avatarUrl = req.body.sender.avatar_url;
   axios
     .post(process.env.DISCORD_WEBHOOK_URL, {
       content: content,
