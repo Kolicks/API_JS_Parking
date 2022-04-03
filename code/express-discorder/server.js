098092a152c8da0c8ec774bda9f64186e32f4ee2
Kolicks <ricardcasas@yahoo.es>
2022-04-03 19:24:33 +0200

    Server DB

--- code/express-discorder/server.js
+++ code/express-discorder/server.js
@@ -2,8 +2,37 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios").default;
 
+//Executem el servidor i client
 const app = express();
 const port = 40300;
+
+//---------------------------------------
+const {InfluxDB} = require('@influxdata/influxdb-client')
+
+// You can generate an API token from the "API Tokens Tab" in the UI
+const token = 'cEG37QZb92xNMRjP9GremXtUN9UZglLXPWrnNiI7EV33aGcwJ4gTivxUE3P1xuCVdQ--sgY9E8BSImmq92vwDg=='
+const org = 'UdG'
+const bucket = 'Temp'
+
+function InfluxDBWrite(data){
+const client = new InfluxDB({url: 'http://localhost:8086', token: token})
+const {Point} = require('@influxdata/influxdb-client')
+const writeApi = client.getWriteApi(org, bucket)
+
+//Escrivim al Influx
+//writeApi.useDefaultTags({host: 'host1'})
+const point = new Point('Temperatura').floatField('Temp', data)
+writeApi.writePoint(point)
+writeApi
+    .close()
+    .then(() => {
+        console.log('FINISHED')
+    })
+    .catch(e => {
+        console.error(e)
+        console.log('Finished ERROR')
+    })}
+//--------------------------------------------
 
 app.use(express.json());
 
@@ -41,32 +70,6 @@ app.post("/github", (req, res) => {
     .catch((err) => console.error(`Error sending to Discord: ${err}`));
 });
 
-//post Lora-Discord
-app.post("/casauplink", (req, res) =>{
-  console.log(req.body.uplink_message.decoded_payload);
-  const username = "LoRa Draginos Node Temp";
-  const dades = req.body.uplink_message.decoded_payload.Temp;
-  const content = `:rocket:${username} fa ${dades}ºC a casa :rocket:`;
-  //const avatarUrl = req.body.sender.avatar_url;
-
-  axios
-    .post(process.env.DISCORD_WEBHOOK_URL_2, {
-      content: content,
-      /*embeds: [
-        {
-          image: {
-            url: avatarUrl,
-          },
-        },
-      ],*/
-    })
-    .then((discordResponse) => {
-      console.log("Success!");
-      res.status(204).send();
-    })
-    .catch((err) => console.error(`Error sending to Discord: ${err}`));
-});
-
 //post LoRa-Postman
 app.post("/postman", async (req, res) =>{
   console.log(req.body.uplink_message.decoded_payload);
@@ -76,9 +79,11 @@ app.post("/postman", async (req, res) =>{
   //const avatarUrl = req.body.sender.avatar_url;
   let number = isNaN(data);
   if (!number){
+    InfluxDBWrite(data);
     axios
       .post(process.env.WEBHOOK_URL_3, {
         content: data,
+
         /*embeds: [
           {
             image: {
@@ -92,6 +97,8 @@ app.post("/postman", async (req, res) =>{
         res.status(200).send();
       })
       .catch((err) => console.error(res.status(500).send('Internal server error')));
+      
+
   }
 });
 
